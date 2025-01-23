fdb02744b617a1c2bf08462e9590df715f35d744
ash-baker3 <ayshanaushad.cn@gmail.com>
2025-01-22 19:53:11 -0500

    calc implementation

---
+++ MyCalculator.java
@@ -0,0 +1,121 @@
+//Java program to build a calculator
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class MyCalculator extends JFrame implements ActionListener {
+    // Components
+    private JTextField inputField;
+    private JButton[] numberButtons = new JButton[10];
+    private JButton addButton, subButton, mulButton, divButton, eqButton, clrButton;
+    private JPanel panel;
+
+    // Variables
+    private double num1 = 0, num2 = 0, result = 0;
+    private char operator;
+
+    public MyCalculator() {
+        // Frame setup
+        setTitle("Calculator");
+        setSize(400, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+
+        // Input field
+        inputField = new JTextField();
+        inputField.setBounds(50, 25, 300, 50);
+        add(inputField);
+
+        // Buttons
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        eqButton = new JButton("=");
+        clrButton = new JButton("C");
+
+        JButton[] functionButtons = {addButton, subButton, mulButton, divButton, eqButton, clrButton};
+        for (JButton button : functionButtons) {
+            button.addActionListener(this);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+        }
+
+        // Panel for buttons
+        panel = new JPanel();
+        panel.setBounds(50, 100, 300, 300);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+
+        // Add buttons to panel
+        for (int i = 1; i <= 9; i++) {
+            panel.add(numberButtons[i]);
+        }
+        panel.add(addButton);
+        panel.add(numberButtons[0]);
+        panel.add(subButton);
+        panel.add(mulButton);
+        panel.add(divButton);
+        panel.add(eqButton);
+        panel.add(clrButton);
+
+        add(panel);
+
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for (int i = 0; i < 10; i++) {
+            if (e.getSource() == numberButtons[i]) {
+                inputField.setText(inputField.getText().concat(String.valueOf(i)));
+            }
+        }
+        if (e.getSource() == addButton) {
+            num1 = Double.parseDouble(inputField.getText());
+            operator = '+';
+            inputField.setText("");
+        }
+        if (e.getSource() == subButton) {
+            num1 = Double.parseDouble(inputField.getText());
+            operator = '-';
+            inputField.setText("");
+        }
+        if (e.getSource() == mulButton) {
+            num1 = Double.parseDouble(inputField.getText());
+            operator = '*';
+            inputField.setText("");
+        }
+        if (e.getSource() == divButton) {
+            num1 = Double.parseDouble(inputField.getText());
+            operator = '/';
+            inputField.setText("");
+        }
+        if (e.getSource() == eqButton) {
+            num2 = Double.parseDouble(inputField.getText());
+            switch (operator) {
+                case '+': result = num1 + num2; break;
+                case '-': result = num1 - num2; break;
+                case '*': result = num1 * num2; break;
+                case '/': 
+                    if (num2 != 0) result = num1 / num2;
+                    else inputField.setText("Error");
+                    return;
+            }
+            inputField.setText(String.valueOf(result));
+        }
+        if (e.getSource() == clrButton) {
+            inputField.setText("");
+        }
+    }
+
+    public static void main(String[] args) {
+        new MyCalculator();
+    }
+}
+
+
